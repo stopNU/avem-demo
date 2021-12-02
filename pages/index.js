41e729d82ff4a321f0e52045147c676422640240
Darktism <michael2004thomsen@protonmail.com>
2021-12-02 17:42:05 +0200

    Add slices test

--- pages/index.js
+++ pages/index.js
@@ -1,42 +1,45 @@
-import React from 'react'
+import React from "react";
 
-import DefaultLayout from 'layouts'
-//import { Header, HomeBanner, SliceZone } from 'components'
+import DefaultLayout from "layouts";
+import SliceZone from "components/slices/SliceZone";
+import styles from "../styles/Home.module.css";
 
-import { Client } from 'utils/prismicHelpers'
+import { Client } from "utils/prismicHelpers";
 
 const HomePage = ({ doc }) => {
   console.log("doc", doc);
   if (doc && doc.data) {
     return (
       <DefaultLayout>
-        <div className='homepage'>
-        {doc.data.page_title[0].text}
-        </div>
+        <main className={styles.main}>
+          <div className="homepage">
+            <h1 className={styles.title}>{doc.data.page_title[0].text}</h1>
+            <SliceZone sliceZone={doc.data.body1} />
+          </div>
+        </main>
       </DefaultLayout>
-    )
+    );
   }
 
   // Call the standard error page if the document was not found
-  return null
-}
+  return null;
+};
 
 export async function getStaticProps({ preview = null, previewData = {} }) {
+  const { ref } = previewData;
 
-  const { ref } = previewData
+  const client = Client();
 
-  const client = Client()
-
-  const doc = await client.getSingle('homepage', ref ? { ref } : null) || {}
+  const doc = (await client.getSingle("homepage", ref ? { ref } : null)) || {};
   //const menu = await client.getSingle('menu', ref ? { ref } : null) || {}
 
   return {
     props: {
       doc,
       //menu,
-      preview
-    }
-  }
+      preview,
+    },
+  };
 }
 
-export default HomePage
+export default HomePage;
